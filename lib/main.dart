cd2eb4c116ac7d2bce8f9829c38d8b351e85b91e
Rachelforce <art.piskov@gmail.com>
2020-09-17 23:57:08 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey[100],
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[600],
+        ),
+        body: Center(
+          child: Image(image: AssetImage("images/diamond.png")),
+        ),
+      ),
+    ),
+  );
+}
